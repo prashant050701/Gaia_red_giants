e1c83743de7f03cacdd289d55c635f1cc9a21e68
Divyansh Srivastava <49075644+prashant050701@users.noreply.github.com>
2024-06-10 20:23:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -246,12 +246,23 @@ data_x = surveys[survey_x5]["data"] if data_source_x5 == "Original" else gaia_da
 data_y = surveys[survey_y5]["data"] if data_source_y5 == "Original" else gaia_data[survey_y5] if data_source_y5 == "Gaia" else tess_data[survey_y5]
 
 fig = go.Figure()
-fig.add_trace(go.Histogram(
+'''fig.add_trace(go.Histogram(
     x=data_x[param_x5], nbinsx=50, name=f"{survey_x5} {data_source_x5}",
     marker=dict(line=dict(color='black', width=1))
 ))
 fig.add_trace(go.Histogram(
     x=data_y[param_y5], nbinsx=50, name=f"{survey_y5} {data_source_y5}",
+    marker=dict(line=dict(color='black', width=1))
+))'''
+
+fig.add_trace(go.Histogram(
+    x=data_x[param_x5], nbinsx=50, name=f"{survey_x5} {data_source_x5}",
+    histnorm='probability density',
+    marker=dict(line=dict(color='black', width=1))
+))
+fig.add_trace(go.Histogram(
+    x=data_y[param_y5], nbinsx=50, name=f"{survey_y5} {data_source_y5}",
+    histnorm='probability density',
     marker=dict(line=dict(color='black', width=1))
 ))
 fig.update_layout(barmode='overlay', title_text='Interactive Distribution Comparison')
