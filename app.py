526bec65dce3ea1e84cda5c8d8ece9e04d4fdb6d
Divyansh Srivastava <49075644+prashant050701@users.noreply.github.com>
2024-06-10 19:40:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -245,11 +245,11 @@ data_y = surveys[survey_y5]["data"] if data_source_y5 == "Original" else gaia_da
 
 fig = go.Figure()
 fig.add_trace(go.Histogram(
-    x=data_x[param_x5], nbinsx=50, name=f"{survey_x5}{data_source_x5}",
+    x=data_x[param_x5], nbinsx=50, name=f"{survey_x5} {data_source_x5}",
     marker=dict(line=dict(color='black', width=1))
 ))
 fig.add_trace(go.Histogram(
-    x=data_y[param_y5], nbinsx=50, name=f"{survey_y5}{data_source_y5}",
+    x=data_y[param_y5], nbinsx=50, name=f"{survey_y5} {data_source_y5}",
     marker=dict(line=dict(color='black', width=1))
 ))
 fig.update_layout(barmode='overlay', title_text='Interactive Distribution Comparison')
