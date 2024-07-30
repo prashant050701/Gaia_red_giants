042d02686cd14ea164fb805d5810dcb249b77b96
Divyansh Srivastava <49075644+prashant050701@users.noreply.github.com>
2024-07-30 16:47:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -100,34 +100,29 @@ surveys = {
 exoplanets = pd.read_csv("database/updated_exoplanet_data.csv")
 exoplanet_gaia_ids = set(exoplanets['Gaia ID'].astype(str))
 
-import numpy as np
-import plotly.express as px
-import streamlit as st
-
 def plot_hr_diagram(data, teff_col, log_l_col, logg_col, title, log_conversion, exoplanet_ids=None, use_cmap=True):
     luminosity = np.log10(data[log_l_col]) if log_conversion else data[log_l_col]
     data['has_exoplanet'] = data['source_id'].astype(str).isin(exoplanet_ids)
 
-    marker_colors = np.where(data['has_exoplanet'], 'red', 'rgba(0,0,0,0)')  # red outline for exoplanet hosts, transparent for others
-
     if use_cmap:
-        fig = px.scatter(data, x=teff_col, y=luminosity, color=logg_col, symbol='has_exoplanet',
-                         color_continuous_scale='Viridis', labels={"color": "logg"}, title=title,
-                         marker=dict(line=dict(color=marker_colors, width=2)))
+        fig = px.scatter(data, x=teff_col, y=luminosity, color=logg_col,
+                         color_continuous_scale='Viridis', labels={"color": "logg"}, title=title)
     else:
-        fig = px.scatter(data, x=teff_col, y=luminosity, title=title,
-                         marker=dict(line=dict(color=marker_colors, width=2)))
-    
+        fig = px.scatter(data, x=teff_col, y=luminosity, title=title)
+
+    fig.update_traces(marker=dict(line=dict(color=np.where(data['has_exoplanet'], 'red', 'rgba(0,0,0,0)'), width=2)))
+
     fig.update_xaxes(title="Teff (K)", autorange="reversed")
-    
+
     if 'TESS' in title:
         fig.update_yaxes(title="V_mag")
     else:
         fig.update_yaxes(title="log(L/Lsun)")
 
-    fig.update_layout(legend=dict(x=0,xanchor='left', y=1, yanchor='top'))
-    
+    fig.update_layout(legend=dict(x=0, xanchor='left', y=1, yanchor='top'))
+
     st.plotly_chart(fig, use_container_width=True)
+
 
         
 def plot_distribution(data, columns, title):
