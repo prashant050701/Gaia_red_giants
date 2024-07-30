9486f37a3ea2c2e21e916e39dee8c80dfaa45e86
Divyansh Srivastava <49075644+prashant050701@users.noreply.github.com>
2024-07-30 15:36:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ exoplanet_gaia_ids = set(exoplanets['Gaia ID'].astype(str))
 def plot_hr_diagram(data, teff_col, log_l_col, logg_col, title, log_conversion, exoplanet_ids=None, use_cmap=True):
     luminosity = np.log10(data[log_l_col]) if log_conversion else data[log_l_col]
     data['has_exoplanet'] = data['source_id'].isin(exoplanet_ids)
-    st.write(data['has_exoplanet'])
+    st.write(exoplanet_gaia_ids)
     st.write(set(data['source_id']))
     
     if use_cmap:
