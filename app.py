63685ecb389a8f79fa859b4bcb96bc218e692f54
Divyansh Srivastava <49075644+prashant050701@users.noreply.github.com>
2024-07-30 14:15:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -57,11 +57,12 @@ surveys = {
 }
 
 exoplanets = pd.read_csv("database/updated_exoplanet_data.csv")
-exoplanet_gaia_ids = set(exoplanets['Gaia ID'])
+exoplanet_gaia_ids = set(exoplanets['Gaia ID'].astype(str))
 
 def plot_hr_diagram(data, teff_col, log_l_col, logg_col, title, log_conversion, exoplanet_ids=None, use_cmap=True):
     luminosity = np.log10(data[log_l_col]) if log_conversion else data[log_l_col]
     data['has_exoplanet'] = data['source_id'].isin(exoplanet_ids)
+    st.write(exoplanet_gaia_ids)
     st.write(set(data['source_id']))
     st.write(data['has_exoplanet']==True)
     
