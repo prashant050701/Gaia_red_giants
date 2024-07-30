0f48a13b1cbb16809e4477bb4b8dcc4f45d42730
Divyansh Srivastava <49075644+prashant050701@users.noreply.github.com>
2024-07-30 13:08:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -61,28 +61,23 @@ exoplanet_gaia_ids = set(exoplanets['Gaia ID'])
 
 def plot_hr_diagram(data, teff_col, log_l_col, logg_col, title, log_conversion, exoplanet_ids=None, use_cmap=True):
     luminosity = np.log10(data[log_l_col]) if log_conversion else data[log_l_col]
-
     data['has_exoplanet'] = data['source_id'].isin(exoplanet_ids)
-
-    exoplanet_hosts = data[data['has_exoplanet']]
-    non_hosts = data[~data['has_exoplanet']]
-
-
-    fig = px.scatter(non_hosts, x=teff_col, y=luminosity, color=logg_col,
-                     color_continuous_scale='Viridis', labels={"color": "logg"}, title=title,
-                     category_orders={"has_exoplanet": [False, True]})
-
-    fig.add_trace(go.Scatter(x=exoplanet_hosts[teff_col], y=luminosity[exoplanet_hosts.index], mode='markers', marker=dict(color='red', size=10, line=dict(color='black', width=2)), name='Exoplanet Hosts'))
-
-    fig.update_traces(showlegend=True)
-
+    
+    if use_cmap:
+        fig = px.scatter(data, x=teff_col, y=luminosity, color=logg_col, symbol='has_exoplanet',
+                         color_continuous_scale='Viridis', labels={"color": "logg"}, title=title)
+    else:
+        fig = px.scatter(data, x=teff_col, y=luminosity, title=title)
+    
     fig.update_xaxes(title="Teff (K)", autorange="reversed")
+    
     if 'TESS' in title:
         fig.update_yaxes(title="V_mag")
     else:
         fig.update_yaxes(title="log(L/Lsun)")
 
-    fig.update_layout(legend=dict(x=0, xanchor='left', y=1, yanchor='top'))
+    fig.update_layout(legend=dict(x=0,xanchor='left', y=1, yanchor='top'))
+    
     st.plotly_chart(fig, use_container_width=True)
 
         
