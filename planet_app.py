36201c5e8f86c644fbcf061ae3e2bb33cda8ecb2
Divyansh Srivastava <49075644+prashant050701@users.noreply.github.com>
2024-08-11 11:46:27 +0200

    Update planet_app.py

--- planet_app.py
+++ planet_app.py
@@ -127,10 +127,10 @@ def plot_occurrence_rates(df, param1, param2, bin_edges_param1, bin_edges_param2
             ax.text(x_center, y_center, f'{percentage_value:.3f}%', color='black', ha='center', va='center', fontsize=10)
 
             if show_error:
-            if scale_param1 == "Linear" and scale_param2 == "Linear":
-                error_y_position = y_center - 0.05 * (bin_edges_param1[i+1] - bin_edges_param1[i])
-            else:
-                error_y_position = y_center - 0.05 * y_center
+                if scale_param1 == "Linear" and scale_param2 == "Linear":
+                    error_y_position = y_center - 0.05 * (bin_edges_param1[i+1] - bin_edges_param1[i])
+                else:
+                    error_y_position = y_center - 0.05 * y_center
 
 
     ax.set_xlabel(param2)
