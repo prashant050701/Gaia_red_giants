14d694fb6d13df46d7074f53f3d22a323036277a
Divyansh Srivastava <49075644+prashant050701@users.noreply.github.com>
2024-08-23 14:20:10 +0200

    Update planet_app.py

--- planet_app.py
+++ planet_app.py
@@ -237,8 +237,8 @@ def section4_main(data_ps_all, data_gg, data_ps_planet):
             #eta_new = 1/eta
             k = 1
             log_eta_new = np.log(k + eta_new)
-            corrected_occ_rate = np.sum(log_eta_new * occ_rate) / np.sum(log_eta_new) if np.sum(log_eta_new) > 0 else 0
-            #corrected_occ_rate = np.sum(eta_new * occ_rate) / np.sum(eta_new) if np.sum(eta_new) > 0 else 0
+            #corrected_occ_rate = np.sum(log_eta_new * occ_rate) / np.sum(log_eta_new) if np.sum(log_eta_new) > 0 else 0
+            corrected_occ_rate = np.sum(eta_new * occ_rate) / np.sum(eta_new) if np.sum(eta_new) > 0 else 0
             #corrected_occ_rate = np.sum(eta * occ_rate) / np.sum(eta) if np.sum(eta) > 0 else 0
             st.write(f"Corrected Occurrence Rate: {corrected_occ_rate:.6f}")
 
