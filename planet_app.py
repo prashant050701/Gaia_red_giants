619193c99415027405904c9f9ad8ca5984d3027b
Divyansh Srivastava <49075644+prashant050701@users.noreply.github.com>
2024-08-23 14:33:40 +0200

    Update planet_app.py

--- planet_app.py
+++ planet_app.py
@@ -237,8 +237,9 @@ def section4_main(data_ps_all, data_gg, data_ps_planet):
             #eta_new = 1/eta
             k = 1
             log_eta_new = np.log(k + eta_new)
+            corrected_occ_rate = np.sum(occ_rate/log_eta_new) / 1/(np.sum(log_eta_new)) if np.sum(log_eta_new) > 0 else 0
             #corrected_occ_rate = np.sum(log_eta_new * occ_rate) / np.sum(log_eta_new) if np.sum(log_eta_new) > 0 else 0
-            corrected_occ_rate = np.sum(eta_new * occ_rate) / np.sum(eta_new) if np.sum(eta_new) > 0 else 0
+            #corrected_occ_rate = np.sum(eta_new * occ_rate) / np.sum(eta_new) if np.sum(eta_new) > 0 else 0
             #corrected_occ_rate = np.sum(eta * occ_rate) / np.sum(eta) if np.sum(eta) > 0 else 0
             st.write(f"Corrected Occurrence Rate: {corrected_occ_rate:.6f}")
 
@@ -246,10 +247,11 @@ def section4_main(data_ps_all, data_gg, data_ps_planet):
             st.markdown("""
     **Corrected Occurrence Rate Formula:**
     $$
+    \\text{Corrected Occurrence Rate} = \\frac{\\sum \\left(\\frac{1}{\\log(k + \\frac{1}{\\eta_i})} \\times N\\_{\\text{Occ}_i}\\right)}{\\sum \\frac{1}{\\log(k + \\frac{1}{\\eta_i})}}
     %\\text{Corrected Occurrence Rate} = \\frac{\\sum \\left(\\log(k + \\frac{1}{\\eta_i}) \\times N\\_{\\text{Occ}_i}\\right)}{\\sum \\log(k + \\frac{1}{\\eta_i})}
     %\\text{Corrected Occurrence Rate} = \\frac{\\sum \\left(\\frac{1}{\\eta_i} \\times N\\_{\\text{Occ}_i}\\right)}{\\sum \\frac{1}{\\eta_i}}
 
-    \\text{Corrected Occurrence Rate} = \\frac{\\sum \\left(\\frac{N\\_{\\text{Occ}_i}}{\\eta_i}\\right)}{\\sum \\left(\\frac{1}{\\eta_i}\\right)}
+    %\\text{Corrected Occurrence Rate} = \\frac{\\sum \\left(\\frac{N\\_{\\text{Occ}_i}}{\\eta_i}\\right)}{\\sum \\left(\\frac{1}{\\eta_i}\\right)}
     $$
     Where:
     - $\\eta_i$ is the efficiency of detection for bin $i$, defined as:
